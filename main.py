574cc29610f0f595a917daaca6636f7a266b66b6
gaolei <365549445@qq.com>
2017-04-08 23:48:10 +0800

    2017.4.8 at home

--- main.py
+++ main.py
@@ -30,6 +30,32 @@ def getjson(sql):
 	else:
 		return res
 
+# def cal(date,base_c,key,val):
+# 	string = ''
+# 	id_i = int(key)
+# 	#val={'salary','another','drawback','benefit01','benefit02','benefit03','benefit04','exam'}
+# 	base_i = int(val['salary'])*(1-int(val['exam'])/100)
+# 	endow_i = base_i*0.05
+# 	endow_c = endow_i*2
+# 	unemploy_i = base_i*0.02
+# 	unemploy_c = unemploy_i*2
+# 	medical_i = base_i*0.01
+# 	medical_c = medical_i*2
+# 	injury_i = base_i*0.01
+# 	injury_c = injury_i*2
+# 	house_i = base_i*0.05
+# 	house_c = hous_i*2
+# 	individual = base_i*0.05
+# 	another = int(val['another'])
+# 	drawback = int(val['drawback'])
+# 	bene01 = int(val['benefit01'])
+# 	bene02 = int(val['benefit02'])
+# 	bene03 = int(val['benefit03'])
+# 	bene04 = int(val['benefit04'])
+# 	actual = base_i-endow_i-unemploy_i-medical_i-injury_i-house_i-individual-another+drawback+bene01+bene02+bene03+bene04
+# 	profit = base_c-base_i-endow_c-unemploy_c-medical_c-injury_c-house_c
+# 	string = "INSERT INTO salary VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,)"
+
 @app.route('/')
 def login_r():
 	return redirect('/signin')
@@ -102,7 +128,8 @@ def com_stuff_add():
 	tele = int(request.form.get('tele'))
 	address = request.form.get('address')
 	company = int(request.form.get('company'))
-	sql = "INSERT INTO stuff(NAME,SEX,AGE,COMPANY,TEL,ADDRESS) VALUES ('%s','%s',%s,%s,'%s','%s')" %(name,sex,age,company,tele,address)
+	salary = int(request.form.get('salary'))
+	sql = "INSERT INTO stuff(NAME,SEX,AGE,COMPANY,TEL,ADDRESS,SALARY) VALUES ('%s','%s',%s,%s,'%s','%s',%s)" %(name,sex,age,company,tele,address,salary)
 	res = conn.execute(sql)
 	if not res:
 		return 'ok'
@@ -112,11 +139,71 @@ def com_stuff_add():
 @app.route('/common/stuff/get')
 def com_stuff_get():
 	company = session['group']
-	sql = 'SELECT ID_I,NAME,SEX,AGE,TEL,ADDRESS FROM stuff WHERE COMPANY=%s' % (company,)
+	sql = 'SELECT ID_I,NAME,SEX,AGE,TEL,ADDRESS,SALARY FROM stuff WHERE COMPANY=%s' % (company,)
 	res = getjson(sql)
 	return res
-	
 
+@app.route('/common/stuff/del',methods=['POST'])
+def com_stuff_del():
+	ID_I = request.form.get('id')
+	sql = "DELETE FROM stuff WHERE ID_I=%s" % (ID_I)
+	res = conn.execute(sql)
+	if not res:
+		return 'ok'
+	else:
+		return 'error'
+
+@app.route('/common/stuff/update',methods=['POST'])
+def com_stuff_upd():
+	ID_I = request.form.get('id')
+	name = request.form.get('name')
+	sex = request.form.get('sex')
+	age = request.form.get('age')
+	tele = request.form.get('tele')
+	address = request.form.get('address')
+	salary = request.form.get('salary')
+	sql = "UPDATE stuff SET NAME='%s',SEX='%s',AGE=%s,TEL='%s',ADDRESS='%s',SALARY=%s WHERE ID_I=%s" %(name,sex,age,tele,address,salary,ID_I)
+	res = conn.execute(sql)
+	if not res:
+		return 'ok'
+	else:
+		return 'error'
+	
+@app.route('/common/salary')
+def com_salary():
+	if 'user' in session:
+		return render_template('salary.html')
+	else:
+		return redirect('/signin')
+
+@app.route('/common/salary/getbase')
+def com_salary_gb():
+	session['salary_dict'] = {}
+	# sql = "SELECT ID_I,NAME,SALARY,ANOTHER,DRAWBACK,BENEFIT01,BENEFIT02,BENEFIT03,BENEFIT04,EXAM FROM stuff f JOIN set_i s ON f.ID_I = s.ID"
+	#创建了一个视图，但是这里要注意视图需要带上company
+	sql = "SELECT ID_I,NAME,SALARY,ANOTHER,DRAWBACK,BENEFIT01,BENEFIT02,BENEFIT03,BENEFIT04,EXAM FROM stuff_set_i WHERE COMPANY=%s" % (session['group'])
+	tmp = conn.execute(sql)
+	for item in tmp:
+ 		session['salary_dict'][item[0]] = {'salary':item[2],'another':item[3],'drawback':item[4],'benefit01':item[5],'benefit02':item[6],'benefit03':item[7],'benefit04':item[8],'exam':item[9]}
+ 	#print session['salary_dict']
+	res = json.dumps(tmp)
+	return res
+
+@app.route('/common/salary/set',methods=['POST'])
+def com_salary_set():
+	ID_I = request.form.get('id')
+	col = request.form.get('col')
+	val = request.form.get('val')
+	#print session['salary_dict'],session会将内容全部转换成unicode
+	session['salary_dict'][ID_I][col]=val
+	print session['salary_dict']
+	sql = "UPDATE set_i SET %s=%s WHERE ID=%s" % (col,val,ID_I)
+	res = conn.execute(sql)
+	if not res:
+		return 'ok'
+	else:
+		return 'error'
+@app.route('/common/salary/cal')
 
 
 if __name__ == '__main__':
